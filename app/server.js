ef08ce0ddafcb7f758ee44270dfa17323f2a6087
Muhammad Ibrahim Nadeem <130080029+MuhammadIbrahimNadeem@users.noreply.github.com>
2025-04-14 13:55:20 +0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -56,7 +56,7 @@ app.get('/welcome', (req, res) => {
 
 // Initialize database on startup
 initDB().then(() => {
-    app.listen(3000, () => {
+    app.listen(3000, '0.0.0.0', () => {
         console.log('Server running on port 3000');
-    });
+    });    
 });
